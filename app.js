650fa9ababc59a7b455666f51b5506b620f9311e
Jeff Maughan <geophray@gmail.com>
2020-04-06 14:45:33 -0700

    My first attempt s On branch master

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // TODO
+var items = ['Milk', 'Bread'];
+var GroceryListItem = (props) => (
+  <li>{props[0]}</li>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    <GroceryListItem />
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <GroceryList />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));

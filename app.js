73caa478d4db14c4533bd98d2e908a9e04248954
Jeff Maughan <geophray@gmail.com>
2020-04-06 16:29:09 -0700

    Add clickable list item

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 
-var GroceryListItem = (props) => (
-    <li>{props.item}</li>
-);
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return (<li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>);
+  }
+
+}
 
 var GroceryList = (props) => (
   <ul>
